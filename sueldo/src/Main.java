11f3bea00da6fe31d968d331c8e55bf29de4513d
DURAN-09 <20243ds173@utez.edu.mx>
2025-09-11 10:49:53 -0600

    create : worker POJO

--- sueldo/src/Main.java
+++ sueldo/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Worker> workers = new ArrayList<>();
+        List<Worker> workersToDelete = new ArrayList<>();
+        workers.add(new Worker(1,"Carlos",1500));
+        workers.add(new Worker(2,"Daniel",900));
+        workers.add(new Worker(3,"Cano",100));
+        workers.add(new Worker(4,"Chema",600));
+        workers.add(new Worker(5,"Juanito",2500));
+        workers.add(new Worker(6,"Pepe",2000));
+        System.out.println("TRABAJADORES"+workers);
+        float total = 0;
+        for(Worker w :workers){
+            total+=w.getSueldo();
+        }
+        float promedio = total/workers.size();
+        System.out.println("Promedio sueldo"+promedio);
+        for(Worker w :workers){
+            //workers.removeIf(worker -> w.getSueldo()<1000);
+           if(w.getSueldo() < 1000){
+               workersToDelete.add(w);
+            }
+        }
+        workers.removeAll(workersToDelete);
+        for(Worker w : workers){
+            System.out.println("empleado con sueldo >= 1000 : "+w.toString());
+        }
 
     }
 }
---
+++ sueldo/src/Worker.java
@@ -0,0 +1,26 @@
+public class Worker {
+    private int id;
+    private String nombre;
+    private float sueldo;
+
+    public Worker(int id, String nombre, float sueldo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public float getSueldo() {
+        return sueldo;
+    }
+    @Override
+    public String toString(){
+        return "worker {id:"+id+"nombre:"+nombre+"sueldo:"+sueldo+" }";
+    }
+}
