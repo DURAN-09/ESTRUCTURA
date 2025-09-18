fda72cbd0c0b27ed11147adbabd4a32432a34418
DURAN-09 <20243ds173@utez.edu.mx>
2025-09-18 08:52:00 -0600

    change : rename package and create fibonacci class

---
+++ estructura/src/Fibonacci/Main.java
@@ -0,0 +1,16 @@
+package Fibonacci;
+
+public class Main {
+
+    public static void main(String[] args){
+        int nNum=10;
+        for(int i = 0;i<nNum;i++){
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    static int fibonacci(int n){
+        if ( n<= 1) return n;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
