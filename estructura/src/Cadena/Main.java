fcb1c40bfd63cbab21d9938c7966f18f98889c4e
DURAN-09 <20243ds173@utez.edu.mx>
2025-09-19 07:52:27 -0600

    create: Cadena class

---
+++ estructura/src/Cadena/Main.java
@@ -0,0 +1,14 @@
+package Cadena;
+
+public class Main {
+    public static void main(String[] args){
+        String firstWord="hola";
+        int l = firstWord.length();
+        System.out.println(Cadena(firstWord,l-1));
+    }
+    static String Cadena(String word,int length){
+        if(length==0) return String.valueOf(word.charAt(length));
+        return word.charAt(length) + Cadena(word,length-1);
+    }
+}
+
