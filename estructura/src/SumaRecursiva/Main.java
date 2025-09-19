101405f28e7f59b32455331e6bedb37bf72c1e35
DURAN-09 <20243ds173@utez.edu.mx>
2025-09-19 07:32:26 -0600

    create: SumaRecursiva class

---
+++ estructura/src/SumaRecursiva/Main.java
@@ -0,0 +1,17 @@
+package SumaRecursiva;
+
+public class Main {
+    public static void main(String[] args){
+        int num=4;
+        if(num<0) return;
+        for (int i = num;i>=0;i--){
+            System.out.println(i);
+        }
+        System.out.println("la suma es "+Suma(num));
+    }
+
+    static int Suma(int num){
+        if(num<=1) return num;
+        return num + Suma(num-1);
+    }
+}
