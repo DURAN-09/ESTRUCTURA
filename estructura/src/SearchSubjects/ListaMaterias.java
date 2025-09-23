36477ab1a83f695a1d43cb9130a7d59400f71111
DURAN-09 <20243ds173@utez.edu.mx>
2025-09-22 20:01:08 -0600

    create: subjects class

---
+++ estructura/src/SearchSubjects/ListaMaterias.java
@@ -0,0 +1,28 @@
+package SearchSubjects;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Queue;
+/*
+- Clase ListaMaterias con métodos: void agregar(String materia), boolean contiene(String materia), void imprimir().
+
+1) Insertar al menos 5 materias.
+2) Solicitar/definir una materia a buscar.
+3) Usar contiene() y mostrar el resultado.
+*/
+public class ListaMaterias {
+    Deque<String> materias;
+    public ListaMaterias(){
+        materias = new ArrayDeque<>();
+    }
+    public void agregar(String materia){
+        materias.push(materia);
+    }
+    public boolean contiene(String materia) {
+        return materias.contains(materia);
+    }
+    public void imprimir() {
+        System.out.println("Materias: " + materias);
+    }
+}
---
+++ estructura/src/SearchSubjects/Main.java
@@ -0,0 +1,21 @@
+package SearchSubjects;
+
+public class Main {
+    public static void main(String[] args){
+        String buscar = "materia4";
+        ListaMaterias lista = new ListaMaterias();
+        lista.agregar("materia1");
+        lista.agregar("materia2");
+        lista.agregar("materia3");
+        lista.agregar("materia4");
+        lista.agregar("materia5");
+
+        lista.imprimir();
+        System.out.println("Buscando:"+buscar);
+        if(lista.contiene(buscar)){
+            System.out.println("Resultado: Materia encontrada");
+        } else {
+            System.out.println("Resultado: Materia no encontrada");
+        }
+    }
+}
