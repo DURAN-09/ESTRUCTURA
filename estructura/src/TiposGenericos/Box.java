7e202acaf6655457f90d8f1d315b83ec4ef3adbc
DURAN-09 <20243ds173@utez.edu.mx>
2025-10-06 09:03:57 -0600

    create: generics and properties class

---
+++ estructura/src/TiposGenericos/Box.java
@@ -0,0 +1,24 @@
+package TiposGenericos;
+
+
+public class Box<T>{
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+
+
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+
+
+}
