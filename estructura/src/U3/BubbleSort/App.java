96640e7ceb014255e5c3a854d58902dc31f97106
DURAN-09 <20243ds173@utez.edu.mx>
2025-11-07 07:59:54 -0600

    add: u3 package and bubblesort class

---
+++ estructura/src/U3/BubbleSort/App.java
@@ -0,0 +1,9 @@
+package U3.BubbleSort;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BubbleSort b = new BubbleSort();
+        int [] arr = {5,1,9,10,3};
+        b.sort(arr);
+    }
+}
---
+++ estructura/src/U3/BubbleSort/BubbleSort.java
@@ -0,0 +1,24 @@
+package U3.BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public void sort(int[] arr){
+        int n = arr.length;
+        //recorremos los elementos del arreglo
+        for(int i = 0; i< n - 1; i++){
+            //Recorremos los elementos adyacentes -1 -i
+            for(int j = 0; j< n - i - 1; j++){
+                //el elemento actual es mayor que el siguiente?
+                if(arr[j] > arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Arreglo ordenado:");
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
